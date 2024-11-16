92e52ed7f913dc61b32638a2c6c653ce1e48e7ec
Mikhail Ivashchenko <52649381+mikhail0090025@users.noreply.github.com>
2024-11-16 15:03:51 +0100

    Interm commit

--- my_social_network/my_social_network/views.py
+++ my_social_network/my_social_network/views.py
@@ -19,7 +19,15 @@ def login(request):
         if request.method == 'GET':
             return render(request, 'login.html')
         elif request.method == 'POST':
-            return HttpResponse("", status=200)
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            print(username)
+            print(password)
+            sql_request = db.execute_query_sync('SELECT password_hash FROM userprofile WHERE username = $1', *[username])
+            if len(sql_request) == 0:
+                return HttpResponse('User with this username was not found', status=400)
+
+            print(sql_request)
         else:
             return HttpResponse(f"Bad request method: {request.method} method is not supported!", status=400)
     except asyncpg.PostgresError as e:
@@ -36,13 +44,17 @@ def registration(request):
             name = request.POST.get('name')
             surname = request.POST.get('surname')
             password = request.POST.get('password')
-            r_password = request.POST.get('r_password')
             birth_date = request.POST.get('birth_date')
             birth_date = datetime.strptime(birth_date, '%Y-%m-%d').date()
             salt = bcrypt.gensalt()
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
             hashed_password = hashed_password.decode('utf-8')
             parameters = [username, hashed_password, name, surname, birth_date]
+            # Check for uniqueness of username
+            sql_result = db.execute_query_sync("SELECT * FROM UserProfile WHERE username = $1", *[username])
+            if len(sql_result) > 0:
+                return HttpResponse(f"User with username {username} does already exists", status=400)
+            #
             db.execute_query_sync("INSERT INTO UserProfile (username, password_hash, name, surname, birth_date) VALUES ($1, $2, $3, $4, $5)",
             *parameters
             )
@@ -50,9 +62,11 @@ def registration(request):
         else:
             return HttpResponse(f"Bad request method: {request.method} method is not supported!", status=400)
     except asyncpg.PostgresError as e:
-        return HttpResponse(f"Postgres database error: {e}", status=500)
+        print(f"Postgres database error: {e}")
+        return HttpResponse(f"Internal server error has occured.", status=500)
     except Exception as e:
-        return HttpResponse(f"Unexpected error: {e}", status=500)
+        print(f"Unexpected error: {e}")
+        return HttpResponse(f"Internal server error has occured.", status=500)
 
 def redirect_to_main(request):
     return redirect('mainpage')
